8606899fac396a1c547616b9e22e86b1c37095ed
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-08-10 22:08:22 +0530

    form changed

--- src/Components/Auth/AuthForm.js
+++ src/Components/Auth/AuthForm.js
@@ -61,7 +61,7 @@ const AuthForm = () => {
           padding: "10px",
           width: "70vw",
           height: "auto",
-          marginTop: "10%",
+          marginTop: "70px",
         }}
       >
         <Form onSubmit={SubmitHandler}>
