d76b0fc2375818cedf452d12092b6a8ea77e400a
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-10-06 17:16:44 +0530

    responsiveness added

--- src/Components/Data/HomeData.js
+++ src/Components/Data/HomeData.js
@@ -1,32 +1,32 @@
 const HomeData = [
   {
     date: "JUL16",
-    place: "DETROIT, MI",
+    place: "DETROIT,MI",
     specialPlace: "DTE ENERGY MUSIC THEATRE",
   },
   {
     date: "JUL19",
-    place: "TORONTO,ON",
+    place: "TORONTO",
     specialPlace: "BUDWEISER STAGE",
   },
   {
     date: "JUL22",
-    place: "BRISTOW, VA",
+    place: "BRISTOW,VA",
     specialPlace: "JIGGY LUBE LIVE",
   },
   {
     date: "JUL29",
-    place: "PHOENIX, AZ",
+    place: "PHOENIX,AZ",
     specialPlace: "AK-CHIN PAVILION",
   },
   {
     date: "AUG 2",
-    place: "LAS VEGAS, NV",
+    place: "LAS VEGAS,NV",
     specialPlace: "T-MOBILE ARENA",
   },
   {
     date: "AUG 7",
-    place: "CONCORD, CA",
+    place: "CONCORD",
     specialPlace: "CONCORD PAVILION",
   },
 ];
